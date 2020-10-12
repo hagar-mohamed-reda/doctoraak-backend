8281d6893b97bb534df120d34aa52ba4c8eeccad
hagar-mohamed-reda <gogo202046@yahoo.com>
2020-10-12 12:19:13 +0200

    patient lab

--- app/Http/Controllers/Api/Patient/PatientLabController.php
+++ app/Http/Controllers/Api/Patient/PatientLabController.php
@@ -73,7 +73,7 @@ class PatientLabController extends Controller
     {
         $filteredLabs = [];
         foreach ($labs as $lab) {
-            if ($lab->lab_insurances->where("insurance_id", $insurance)->count() > 0) {
+            if (optional($lab->lab_insurances)->where("insurance_id", $insurance)->count() > 0) {
                 $filteredLabs[] = $lab;
             }
         }
@@ -109,10 +109,10 @@ class PatientLabController extends Controller
     }
 
     public function createLabOrder(Request $request)
-    {   
-        
+    {
+
           $orderdetails = json_decode($request->orderDetails);
-       
+
         $validator = validator()->make(
             $request->all(),
             [
@@ -134,7 +134,7 @@ class PatientLabController extends Controller
         if ($request->has("orderDetails")) {
             $messsage = str_replace("n", Settings::find(5)->value, Message::$MAX_ORDER__DETAILS_NUMBER);
             $messsage_en = str_replace("x", Settings::find(5)->value, Message::$MAX_ORDER__DETAILS_NUMBER_EN);
-         
+
             if (count($orderdetails) > Settings::find(5)->value) {
                 return Message::error($messsage, null,$messsage_en);
             }
@@ -150,7 +150,7 @@ class PatientLabController extends Controller
 
             if (!$workingHours)
                 return Message::error(Message::$DAY_OFF, null,Message::$DAY_OFF_EN);
-            
+
             if ($workingHours->active != 1) {
                 return Message::error(Message::$DAY_OFF, null,Message::$DAY_OFF_EN);
             }
@@ -192,39 +192,39 @@ class PatientLabController extends Controller
             $city_en = City::find($labOrder->lab->city)? City::find($labOrder->lab->city)->name : null;
             $area_ar = Area::find($labOrder->lab->area)? Area::find($labOrder->lab->area)->name_ar : null;
             $area_en = Area::find($labOrder->lab->area)? Area::find($labOrder->lab->area)->name : null;
-            
+
             $message = str_replace("patient", $labOrder->patient->name, Message::$LAB_ORDER);
             $message = str_replace("number", $orderNumber, $message);
             $message = str_replace("name", $labOrder->lab->name, $message);
             $message = str_replace("phone", $labOrder->lab->phone, $message);
             $message = str_replace("city", $city_ar, $message);
             $message = str_replace("area", $area_ar, $message);
-           
+
             $message_en = str_replace("patient", $labOrder->patient->name, Message::$LAB_ORDER_EN);
             $message_en = str_replace("numbers", $orderNumber, $message_en);
             $message_en = str_replace("name", $labOrder->lab->name, $message_en);
             $message_en = str_replace("phones", $labOrder->lab->phone, $message_en);
             $message_en = str_replace("citys", $city_en, $message_en);
             $message_en = str_replace("areas", $area_en, $message_en);
-           
+
             /////notification to patients /////////////////////////
             $title_ar = "بيانات الحجز الخاص بك ".Patient::find($request->patient_id)->name;
             $title_en = "your reservation details ".Patient::find($request->patient_id)->name;
             Patient::notify($title_ar,  $title_en,$message,  $message_en, $icon='icon.png',$request->patient_id, $labOrder->id, "LAB");
-            
-            
-            
-            ///////////////notification for lab 
-            
-           
-    
+
+
+
+            ///////////////notification for lab
+
+
+
             $message_ar_l ="لقد قام   ".Patient::find($request->patient_id)->name." بالحجز لديك و رقم هانفه " .Patient::find($request->patient_id)->phone;
             $message_en_l = "you have new book from ". Patient::find($request->patient_id)->name . "has phone number is" . Patient::find($request->patient_id)->phone ;
              $title_ar_l = "بيانات الحجز ";
             $title_en_l = "Reservation details";
            Lab::notify($title_ar_l,  $title_en_l,$message_ar_l,  $message_en_l, $icon='icon.png',$request->lab_id, $labOrder->id);
-        
-        
+
+
 
             return Message::success($message, null,  $message_en);
         } catch (\Exception $ex) {
