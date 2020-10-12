b6a87a004c0de75d6d0d9e70985a48c9eaa05495
hagar-mohamed-reda <gogo202046@yahoo.com>
2020-10-12 12:30:10 +0200

    lab response

--- app/Lab.php
+++ app/Lab.php
@@ -14,7 +14,7 @@ class Lab extends Model
 
     protected $table = 'labs';
 
-    protected $fillable = [ 
+    protected $fillable = [
         'name',
         'name_ar',
         'name_fr',
@@ -35,10 +35,10 @@ class Lab extends Model
         'lab_doctor_id',
         'photo'
     ];
-    
-    
-    
-    
+
+
+
+
     /**
      * The attributes that are mass assignable.
      *
@@ -47,25 +47,35 @@ class Lab extends Model
     protected $appends = [
         'insurance_company', 'url', 'insurance_id'
     ];
-    
+
+    public function getJson()
+    {
+        $this->photo = url('/image/lab/') . "/" . Lab::find($this->id)->photo;
+        $this->lab_insurances = $this->lab_insurances;
+        $this->working_hours = $this->working_hours()->get();
+
+
+        return $this;
+    }
+
     public function getInsuranceIdAttribute() {
         return implode(',', $this->lab_insurances()->select('insurance_id')->distinct()->get(['insurance_id'])->pluck('insurance_id')->toArray());
     }
-     
-    
+
+
     public function getUrlAttribute() {
         if (!$this->photo || $this->photo == 'lab.png') {
             return url('/image/icon/doctor.png');
         }
-        
+
         return url("image/lab/" . $this->photo);
     }
-    
+
     public function getInsuranceCompanyAttribute() {
         $ids = $this->lab_insurances()->pluck("insurance_id");
         return Insurance::whereIn("id", $ids)->get();
     }
-    
+
     public function doctor()
     {
         return $this->belongsTo('App\LabDoctor', 'lab_doctor_id');
@@ -80,18 +90,18 @@ class Lab extends Model
     {
         return $this->hasMany('App\LabWorkingHours','lab_id');
     }
- 
+
     public function getChartData() {
         $data = [];
         $dates = DB::select("select DISTINCT created_at from lab_orders where lab_id=" . $this->id . " order by created_at ");
-        
+
         foreach($dates as $date) {
             $data[$date->created_at] = LabOrder::whereBetween("created_at", [$date->created_at, $date->created_at])->where("lab_id", $this->id)->count();
         }
-        
+
         return $data;
     }
-    
+
     public function lab_insurances()
     {
         return $this->hasMany('App\LabInsurance');
@@ -108,15 +118,6 @@ class Lab extends Model
 
         return $insuranceNames;
     }
-     public function getJson()
-    {
-        $this->photo = url('/image/lab/') . "/" . Lab::find($this->id)->photo;
-        $this->lab_insurances = $this->lab_insurances;
-        $this->working_hours = $this->working_hours()->get();
-      
-
-        return $this;
-    }
     public static function notify($title_ar, $title_en,$message_ar, $message_en, $icon, $userId, $orderId=null) {
         $userType = "LAB";
         try{
@@ -130,39 +131,39 @@ class Lab extends Model
                 "order_id"=> $orderId,
                "user_type"=>$userType
                 ]);
-                
-                      
+
+
             $data = [
                 "title_ar" => $title_ar,
                 "title_en" => $title_en,
                 "body_ar" => $message_ar,
-                "body_en" => $message_en,    
+                "body_en" => $message_en,
             ];
-            
+
             $token = [
                 Lab::find($userId)->firebase_token
             ];
-                
+
             return Helper::firebaseNotification($token, $data);
-         
-            
+
+
         }catch(\Exception $e){}
     }
-    
+
     public function city() {
         return $this->belongsTo('App\City', 'city_id');
     }
-    
+
     public function area() {
         return $this->belongsTo('App\Area', 'area_id');
     }
-    
-    
+
+
     /**
      * build view object this will make view html
      *
-     * @return ViewBuilder 
-     *       
+     * @return ViewBuilder
+     *
      */
     public function getViewBuilder() {
         $builder = new ViewBuilder($this, "rtl");
@@ -185,13 +186,13 @@ class Lab extends Model
         ];
         foreach (LabDoctor::get(['id', 'name']) as $item)
             $doctors[] = [$item->id, $item->name];
-         
 
-        $insurances = [ 
+
+        $insurances = [
         ];
         foreach (Insurance::get(['id', 'name']) as $item)
             $insurances[] = [$item->id, $item->name];
- 
+
         $builder->setAddRoute(url('/lab/store'))
                 ->setEditRoute(url('/lab/update') . "/" . $this->id)
                 ->setCol(["name" => "id", "label" => __('id'), "editable" => false])
@@ -205,13 +206,13 @@ class Lab extends Model
                 ->setCol(["name" => "city_id", "label" => __('city'), "type" => "select", "data" => $cities])
                 ->setCol(["name" => "area_id", "label" => __('area'), "type" => "select", "data" => $areas])
                 ->setCol(["name" => "map", "label" => __('location'), "type" => "map"])
-                ->setCol(["name" => "photo", "label" => __('photo'), "type" => "image", "required" => false]) 
-                ->setCol(["name" => "email", "label" => __('email'), 'type' => "email", "required" => false]) 
+                ->setCol(["name" => "photo", "label" => __('photo'), "type" => "image", "required" => false])
+                ->setCol(["name" => "email", "label" => __('email'), 'type' => "email", "required" => false])
                 ->setCol(["name" => "insurance_id", "label" => __('insurance_companies'), "type" => "multi_select", "data" => $insurances])
                 ->setCol(["name" => "active", "label" => __('active'), "type" => "checkbox"])
                 ->setCol(["name" => "delivery", "label" => __('delivery'), "type" => "checkbox"])
                 ->setCol(["name" => "available_days", "label" => __('available_days'), 'type' => "number"])
-                 
+
                 ->setUrl(url('/image/lab'))
                 ->build();
 
