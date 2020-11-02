0b835df08a9eb4e9a8b148a37f689d094e188631
hagar-mohamed-reda <gogo202046@yahoo.com>
2020-11-02 16:02:20 +0200

    accept

--- app/PharmacyOrder.php
+++ app/PharmacyOrder.php
@@ -8,7 +8,7 @@ use App\helper\Helper;
 class  PharmacyOrder extends Model
 {
 
-   
+
     protected $table = 'pharmacy_orders';
     protected $fillable = [
         'pharmacy_id',
@@ -18,6 +18,17 @@ class  PharmacyOrder extends Model
 
 
     ];
+
+    protected $appends = [
+        'accept'
+    ];
+
+    public function getAcceptAttribute() {
+        $ar = $this->getAcceptedRequest();
+
+        return $ar? 1 : 0;
+    }
+
     public function patient()
     {
         return $this->belongsTo('App\Patient', 'patient_id');
@@ -27,16 +38,16 @@ class  PharmacyOrder extends Model
     {
         return $this->belongsTo('App\Pharmacy', 'pharmacy_id');
     }
-    
+
     public function pharmacy_requests()
     {
         return $this->hasMany('App\PharmacyRequest');
     }
-    
+
     public function getAcceptedPharmacy() {
         return $this->getAcceptedRequest()->pharmacy;
     }
-    
+
     public function getAcceptedRequest() {
         return $this->pharmacy_requests->where("accept", '1')->first();
     }
@@ -45,12 +56,12 @@ class  PharmacyOrder extends Model
     {
         return $this->hasMany('App\PharmacyOrderDetails', 'pharmacy_order');
     }
-    
+
      public function getJson()
     {
         $this->patient = $this->patient()->first();
         // $this->pharmacy = $this->pharmacy_requests()->first();
-      
+
         $this->photo = url('/image/pharmacyorder') . "/" . $this->photo;
         $this->details = Helper::jsonFilter($this->pharmacy_order_details()->get());
 
