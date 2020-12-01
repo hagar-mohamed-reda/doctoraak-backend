949c349c1785223c3926df72ccf27c71a4423e3b
hagar-mohamed-reda <gogo202046@yahoo.com>
2020-12-01 09:02:19 +0200

    reservation

--- resources/views/patient/action.blade.php
+++ resources/views/patient/action.blade.php
@@ -1,19 +1,19 @@
 <div style="width: 300px" >
-    
+
     <i class="fa fa-edit w3-text-orange w3-button" onclick="edit('{{ url('/patient/edit') . '/' . $patient->id }}')" ></i>
- 
+
     <i class="fa fa-desktop w3-text-green w3-button" onclick="showPage('patient/show/' + '{{ $patient->id }}')" ></i>
-   
+
     <i class="fa fa-trash w3-text-red w3-button" onclick="remove('', '{{ url('/patient/remove/') .'/' . $patient->id }}')" ></i>
-    
+
     @if ($patient->api_token)
     <i class="fa fa-book w3-text-purple w3-button"
         data-toggle="modal"
         data-target="#orderModal{{ $patient->id }}" > {{ __('book') }} </i>
-    
+
     @endif
-    
-</div> 
+
+</div>
 
 <!-- add modal -->
 <div class="modal fade"   role="dialog" id="orderModal{{ $patient->id }}" style="width: 100%!important;height: 100%!important" >
@@ -25,31 +25,31 @@
             </div>
             <div class="modal-body">
                 <ul class="w3-ul" >
-                    
+
                     <li class="w3-padding cursor"  onclick="showPage('clinicorder/create?api_token={{ $patient->api_token }}&patient_id=' + '{{ $patient->id }}')"  >
                         <div class="w3-round w3-white w3-border-gray w3-border" >
                             <img src="{{ url('/image/icon/doctor.png') }}" height="40px" class="shadow w3-padding w3-round w3-blue"  >
                             <b class="w3-padding" >{{ __('book a reservation') }}</b>
                         </div>
                     </li>
-                    
-                    <li class="w3-padding cursor"  onclick="showPage('laborder/create?api_token={{ $patient->api_token }}&patient_id=' + '{{ $patient->id }}')"  >
+
+                    <li class="w3-padding cursor hidden"  onclick="showPage('laborder/create?api_token={{ $patient->api_token }}&patient_id=' + '{{ $patient->id }}')"  >
                         <div class="w3-round w3-white w3-border-gray w3-border" >
                             <img src="{{ url('/image/icon/lab.png') }}" height="40px" class="shadow w3-padding w3-round w3-red"  >
                             <b class="w3-padding" >{{ __('book for lab') }}</b>
                         </div>
                     </li>
-                    
-                    <li class="w3-padding cursor"  onclick="showPage('radiologyorder/create?api_token={{ $patient->api_token }}&patient_id=' + '{{ $patient->id }}')"  >
+
+                    <li class="w3-padding cursor hidden"  onclick="showPage('radiologyorder/create?api_token={{ $patient->api_token }}&patient_id=' + '{{ $patient->id }}')"  >
                         <div class="w3-round w3-white w3-border-gray w3-border" >
                             <img src="{{ url('/image/icon/x-ray.png') }}" height="40px" class="shadow w3-padding w3-round w3-cyan"  >
                             <b class="w3-padding" >{{ __('book for radiology') }}</b>
                         </div>
                     </li>
-                     
-                    
+
+
                 </ul>
-                
+
             </div>
             <div class="modal-footer" >
                 <center>
@@ -69,8 +69,8 @@
     <a href="#" onclick="sendUrl('{{ url('/') }}/patient/active/{{ $patient->id }}', null, this)"  class="w3-text-green w3-button"   >
         <i class="fa fa-info-circle" ></i> <span>تفعيل</span>
     </a>
-    @endif 
-    
+    @endif
+
   <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
     <span class="fa fa-bars"></span>
   </button>
@@ -80,35 +80,35 @@
               <i class="fa fa-desktop" style="margin: 5px" ></i>
               {{ __('show') }}
           </a>
-      </li> 
+      </li>
       @if ($patient->active == 1)
       <li>
           <a href="#" onclick="sendUrl('{{ url('/') }}/patient/deactive/{{ $patient->id }}', null, this)" >
               <i class="fa fa-info-circle" style="margin: 5px" ></i>
               {{ __('deactive') }}
           </a>
-      </li> 
+      </li>
       @else
       <li>
           <a href="#" onclick="sendUrl('{{ url('/') }}/patient/deactive/{{ $patient->id }}', null, this)" >
               <i class="fa fa-info-circle" style="margin: 5px" ></i>
               {{ __('active') }}
           </a>
-      </li> 
-      @endif 
-      
+      </li>
+      @endif
+
       <li>
           <a href="#" onclick="edit('{{ url('/patient/edit') . '/' . $patient->id }}')" >
               <i class="fa fa-desktop" style="margin: 5px" ></i>
               {{ __('show') }}
           </a>
-      </li> 
+      </li>
       <li>
           <a href="#" onclick="edit('{{ url('/patient/edit') . '/' . $patient->id }}')" >
               <i class="fa fa-desktop" style="margin: 5px" ></i>
               {{ __('show') }}
           </a>
-      </li> 
+      </li>
     <li><a href="#">Separated link</a></li>
   </ul>
 </div>
